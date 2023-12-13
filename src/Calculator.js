14754b030287e4f438304015e88701fc86ab76d7
Brian4755 <brian.mccune1@yahoo.com>
2023-12-13 05:36:58 -0600

    calculator prop and button base setup

---
+++ src/Calculator.js
@@ -0,0 +1,25 @@
+const Calculator = () => {
+  return ( 
+    <div className="calculator-grid">
+      <div className="calculator-input"></div>
+      <div className="calculator-button">7</div>
+      <div className="calculator-button">8</div>
+      <div className="calculator-button">9</div>
+      <div className="calculator-button">X</div>
+      <div className="calculator-button">4</div>
+      <div className="calculator-button">5</div>
+      <div className="calculator-button">6</div>
+      <div className="calculator-button">/</div>
+      <div className="calculator-button">1</div>
+      <div className="calculator-button">2</div>
+      <div className="calculator-button">3</div>
+      <div className="calculator-button">+</div>
+      <div className="calculator-button">=</div>
+      <div className="calculator-button">0</div>
+      <div className="calculator-button">C</div>
+      <div className="calculator-button">-</div>
+    </div>
+   );
+}
+ 
+export default Calculator;
