fbf44b1f2d4f3daf1cb1573eed6eb47bd05c7c94
Brian4755 <brian.mccune1@yahoo.com>
2023-12-13 05:56:46 -0600

    grid positioning and template setup

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,23 +1,27 @@
 const Calculator = () => {
   return ( 
-    <div className="calculator-grid">
-      <div className="calculator-input"></div>
-      <div className="calculator-button">7</div>
-      <div className="calculator-button">8</div>
-      <div className="calculator-button">9</div>
-      <div className="calculator-button">X</div>
-      <div className="calculator-button">4</div>
-      <div className="calculator-button">5</div>
-      <div className="calculator-button">6</div>
-      <div className="calculator-button">/</div>
-      <div className="calculator-button">1</div>
-      <div className="calculator-button">2</div>
-      <div className="calculator-button">3</div>
-      <div className="calculator-button">+</div>
-      <div className="calculator-button">=</div>
-      <div className="calculator-button">0</div>
-      <div className="calculator-button">C</div>
-      <div className="calculator-button">-</div>
+    <div className="calculator">
+      <div className="calculator-grid">
+        <div className="calculator-output">
+          <div></div>
+        </div>
+        <button className="calculator-button">7</button>
+        <button className="calculator-button">8</button>
+        <button className="calculator-button">9</button>
+        <button className="calculator-button">X</button>
+        <button className="calculator-button">4</button>
+        <button className="calculator-button">5</button>
+        <button className="calculator-button">6</button>
+        <button className="calculator-button">/</button>
+        <button className="calculator-button">1</button>
+        <button className="calculator-button">2</button>
+        <button className="calculator-button">3</button>
+        <button className="calculator-button">+</button>
+        <button className="calculator-button">=</button>
+        <button className="calculator-button">0</button>
+        <button className="calculator-button">C</button>
+        <button className="calculator-button">-</button>
+      </div>
     </div>
    );
 }
