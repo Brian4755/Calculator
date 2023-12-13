87d39af475e4ec40b557cae3c19dae5e1f12f3e1
Brian4755 <brian.mccune1@yahoo.com>
2023-12-13 10:13:13 -0600

    functionality for button 9

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,13 +1,33 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case '9':
+      return {
+        output: state.output + '9'
+      }
+    default:
+      return state
+  }
+}
+
 const Calculator = () => {
+
+  const[state, dispatch] = useReducer(reducer, { output: 0})
+
+  function input9() {
+    dispatch({type: '9'})
+  }
+
   return ( 
     <div className="calculator">
       <div className="calculator-grid">
         <div className="calculator-output">
-          <div></div>
+          {state.output}
         </div>
         <button className="calculator-button">7</button>
         <button className="calculator-button">8</button>
-        <button className="calculator-button">9</button>
+        <button className="calculator-button" onClick={input9}>9</button>
         <button className="calculator-button">X</button>
         <button className="calculator-button">4</button>
         <button className="calculator-button">5</button>
