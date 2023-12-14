208ee9ac84cec28dd69f52fd3f1f2cc5c370cbe1
Brian4755 <brian.mccune1@yahoo.com>
2023-12-14 08:42:15 -0600

    evaluate functionality and case reducer

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,7 +3,6 @@ import Button from "./Button";
 import Operation from "./Operation";
 
 function reducer(state, {type, payload}) {
-  console.log(state.output)
   switch (type) {
     case 'add-digit':
       if (state.output.length === 0 && payload.digit === 0) {
@@ -11,35 +10,45 @@ function reducer(state, {type, payload}) {
       } if (payload.digit === '.' && state.output.includes('.')) {
         return state
       } else return {
+        ...state,
         output: state.output + `${payload.digit}`
       }
-      case 'plus':
-        return {
-          
-        }
-      case 'clear':
-        return {
-          output: ''
-        }
+    case 'operation':
+      if (state.output.length === 0 || state.prev) {
+        return state
+      } else return {
+        prev: state.output + `${payload.operation}`,
+        output: ''
+      }
+    case 'clear':
+      return {
+        output: ''
+      }
+    case 'evaluate':
+      console.log(state.prev, state.output)
+      return {
+        output: parseInt(state.prev) + parseInt(state.output)
+      }
     default:
       return state
   }
 }
 
+
 const Calculator = () => {
 
-  const[state, dispatch] = useReducer(reducer, { output: ''})
+  const[state, dispatch] = useReducer(reducer, { output: '', prev: ''})
 
   return ( 
     <div className="calculator">
       <div className="calculator-grid">
         <div className="calculator-output">
-          {state.output}
+          <div>{state.prev}</div>
+          <div>{state.output}</div>
         </div>
         <Button digit={7} dispatch={dispatch}/>
         <Button digit={8} dispatch={dispatch}/>
         <Button digit={9} dispatch={dispatch}/>
-        {/* <button className="calculator-button">X</button> */}
         <Operation operation={'x'} dispatch={dispatch}/>
         <Button digit={4} dispatch={dispatch}/>
         <Button digit={5} dispatch={dispatch}/>
@@ -48,9 +57,8 @@ const Calculator = () => {
         <Button digit={1} dispatch={dispatch}/>
         <Button digit={2} dispatch={dispatch}/>
         <Button digit={3} dispatch={dispatch}/>
-        {/* <button className="calculator-button">+</button> */}
         <Operation operation={'+'} dispatch={dispatch}/>
-        <button className="calculator-button">=</button>
+        <button onClick={() => dispatch({type: 'evaluate'})}>=</button>
         <Button digit={0} dispatch={dispatch}/>
         <button onClick={() => dispatch({type: 'clear'})}>C</button>
         <Button digit={'.'} dispatch={dispatch}/>
